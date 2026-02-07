0f9bdb0485105e1ca6ad370258f2c1bf815f2486
상현 <sanghyeon@sanghyeon-ui-Macmini.local>
2026-02-08 03:42:22 +0900

    Fix: 'use client' 작동을 위해 정적 export 제거

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  output: 'export',
-  distDir: 'out',
 };
 
 module.exports = nextConfig;
